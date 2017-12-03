a15580fb2b0204d0035c999242f64561b8885f4a
cxz456999 <cxz456999@gmail.com>
2017-12-03 17:41:03 +0800

    0

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-<?php header( 'Location: public/basic.html' ) ;  ?>
+<?php
+    header('Access-Control-Allow-Origin: https://140.135.52.97');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
+    header( 'Location: public/basic.html' ) ;
+?>
